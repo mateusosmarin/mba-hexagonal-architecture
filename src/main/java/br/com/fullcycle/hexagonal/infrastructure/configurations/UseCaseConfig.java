ad66943252e184d984cd22664261cc9852675f51
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2024-08-24 20:19:08 -0300

    refactor: add use case beans

---
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/configurations/UseCaseConfig.java
@@ -0,0 +1,60 @@
+package br.com.fullcycle.hexagonal.infrastructure.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.fullcycle.hexagonal.application.usecases.CreateCustomerUseCase;
+import br.com.fullcycle.hexagonal.application.usecases.CreateEventUseCase;
+import br.com.fullcycle.hexagonal.application.usecases.CreatePartnerUseCase;
+import br.com.fullcycle.hexagonal.application.usecases.GetCustomerByIdUseCase;
+import br.com.fullcycle.hexagonal.application.usecases.GetPartnerByIdUseCase;
+import br.com.fullcycle.hexagonal.application.usecases.SubscribeCustomerToEventUseCase;
+import br.com.fullcycle.hexagonal.infrastructure.services.CustomerService;
+import br.com.fullcycle.hexagonal.infrastructure.services.EventService;
+import br.com.fullcycle.hexagonal.infrastructure.services.PartnerService;
+
+@Configuration
+public class UseCaseConfig {
+    private final CustomerService customerService;
+    private final EventService eventService;
+    private final PartnerService partnerService;
+
+    public UseCaseConfig(
+            final CustomerService customerService,
+            final EventService eventService,
+            final PartnerService partnerService) {
+        this.customerService = customerService;
+        this.eventService = eventService;
+        this.partnerService = partnerService;
+    }
+
+    @Bean
+    public CreateCustomerUseCase createCustomerUseCase() {
+        return new CreateCustomerUseCase(customerService);
+    }
+
+    @Bean
+    public GetCustomerByIdUseCase getCustomerByIdUseCase() {
+        return new GetCustomerByIdUseCase(customerService);
+    }
+
+    @Bean
+    public CreateEventUseCase createEventUseCase() {
+        return new CreateEventUseCase(eventService, partnerService);
+    }
+
+    @Bean
+    public CreatePartnerUseCase createPartnerUseCase() {
+        return new CreatePartnerUseCase(partnerService);
+    }
+
+    @Bean
+    public GetPartnerByIdUseCase getPartnerByIdUseCase() {
+        return new GetPartnerByIdUseCase(partnerService);
+    }
+
+    @Bean
+    public SubscribeCustomerToEventUseCase subscribeCustomerToEventUseCase() {
+        return new SubscribeCustomerToEventUseCase(customerService, eventService);
+    }
+}
