36d0c064caa2dfc79971fc10ae2bb4b1a5397c75
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2024-08-24 20:30:14 -0300

    test: add create customer use case integration test

---
+++ src/test/java/br/com/fullcycle/hexagonal/application/usecases/CreateCustomerUseCaseIT.java
@@ -0,0 +1,97 @@
+package br.com.fullcycle.hexagonal.application.usecases;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import br.com.fullcycle.hexagonal.IntegrationTest;
+import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
+import br.com.fullcycle.hexagonal.infrastructure.models.Customer;
+import br.com.fullcycle.hexagonal.infrastructure.repositories.CustomerRepository;
+import br.com.fullcycle.hexagonal.infrastructure.services.CustomerService;
+
+public class CreateCustomerUseCaseIT extends IntegrationTest {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @AfterEach
+    void tearDown() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Deve criar um cliente")
+    public void testCreate() {
+        // given
+        final var expectedCPF = "12345678901";
+        final var expectedEmail = "john.doe@gmail.com";
+        final var expectedName = "John Doe";
+
+        final var input = new CreateCustomerUseCase.Input(expectedCPF, expectedEmail, expectedName);
+
+        // when
+        final var useCase = new CreateCustomerUseCase(customerService);
+        final var output = useCase.execute(input);
+
+        // then
+        Assertions.assertNotNull(output.id());
+        Assertions.assertEquals(expectedCPF, output.cpf());
+        Assertions.assertEquals(expectedEmail, output.email());
+        Assertions.assertEquals(expectedName, output.name());
+    }
+
+    @Test
+    @DisplayName("Não deve cadastrar um cliente com CPF duplicado")
+    public void testCreateWithDuplicatedCPFShouldFail() {
+        // given
+        final var expectedCPF = "12345678901";
+        final var expectedEmail = "john.doe@gmail.com";
+        final var expectedName = "John Doe";
+        final var expectedError = "Customer already exists";
+
+        createCustomer(expectedCPF, "john.doe@hotmail.com", expectedName);
+
+        final var input = new CreateCustomerUseCase.Input(expectedCPF, expectedEmail, expectedName);
+
+        // when
+        final var useCase = new CreateCustomerUseCase(customerService);
+        final var actualException = Assertions.assertThrows(ValidationException.class, () -> useCase.execute(input));
+
+        // then
+        Assertions.assertEquals(expectedError, actualException.getMessage());
+    }
+
+    @Test
+    @DisplayName("Não deve cadastrar um cliente com e-mail duplicado")
+    public void testCreateWithDuplicatedEmailShouldFail() {
+        // given
+        final var expectedCPF = "12345678901";
+        final var expectedEmail = "john.doe@gmail.com";
+        final var expectedName = "John Doe";
+        final var expectedError = "Customer already exists";
+
+        createCustomer("12345678902", expectedEmail, expectedName);
+
+        final var input = new CreateCustomerUseCase.Input(expectedCPF, expectedEmail, expectedName);
+
+        // when
+        final var useCase = new CreateCustomerUseCase(customerService);
+        final var actualException = Assertions.assertThrows(ValidationException.class, () -> useCase.execute(input));
+
+        // then
+        Assertions.assertEquals(expectedError, actualException.getMessage());
+    }
+
+    private Customer createCustomer(String cpf, String email, String name) {
+        final var aCustomer = new Customer();
+        aCustomer.setCpf(cpf);
+        aCustomer.setEmail(email);
+        aCustomer.setName(name);
+        return customerRepository.save(aCustomer);
+    }
+}
